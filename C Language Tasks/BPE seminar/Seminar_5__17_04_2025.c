91b2c227832f30fc78d21e558fcbb48246611065
DiMitko <dimitko216@gmail.com>
2025-04-18 14:58:41 +0300

    dootrabotvane

--- C Language Tasks/BPE seminar/Seminar_5__17_04_2025.c
+++ C Language Tasks/BPE seminar/Seminar_5__17_04_2025.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
+//1
+/*
 int main() {
     int guests;
     
@@ -62,3 +65,120 @@ int main() {
 
     return 0;
 }
+*/
+
+//2
+/**/
+
+
+//3
+/**/
+
+//4
+/**/
+
+//5
+/*
+#define MAX_WORD_LEN 100
+
+int main() {
+    char word[] = "tree";
+    int word_len = strlen(word);
+    char display[MAX_WORD_LEN];
+    int guessed[26] = {0};
+    int guesses = 0;
+    int correct_guesses = 0;
+
+    for (int i = 0; i < word_len; i++) {
+        display[i] = '_';
+    }
+    display[word_len] = '\0';
+
+    printf("Welcome to Hangman!\n");
+    printf("Guess the word: %s (Length: %d)\n", display, word_len);
+
+    while (correct_guesses < word_len) {
+        char guess;
+        int found = 0;
+
+        printf("Enter a letter: ");
+        scanf(" %c", &guess);
+        guess = tolower(guess);
+
+        if (!isalpha(guess)) {
+            printf("Please enter a valid letter (a-z).\n");
+            continue;
+        }
+
+        if (guessed[guess - 'a']) {
+            printf("You've already guessed that letter.\n");
+            continue;
+        }
+
+        guessed[guess - 'a'] = 1;
+        guesses++;
+
+        for (int i = 0; i < word_len; i++) {
+            if (word[i] == guess && display[i] == '_') {
+                display[i] = guess;
+                found = 1;
+                correct_guesses++;
+            }
+        }
+
+        if (found) {
+            printf("Good job! Current word: %s\n", display);
+        } else {
+            printf("Sorry, no '%c'. Current word: %s\n", guess, display);
+        }
+    }
+
+    printf("You guessed the word: %s\n", word);
+    printf("Total letters entered: %d\n", guesses);
+
+    if (guesses <= word_len + 2) {
+        printf("Congratulations! You win!\n");
+    } else {
+        printf("You guessed the word, but exceeded the allowed number of guesses. Try again!\n");
+    }
+
+    return 0;
+}
+*/
+
+//6
+/*
+int isAnagram(const char *str1, const char *str2) {
+    int count[26] = {0};
+    int i;
+
+    if (strlen(str1) != strlen(str2)) return 0;
+
+    for (i = 0; str1[i] != '\0'; i++) {
+        if (isalpha(str1[i])) count[tolower(str1[i]) - 'a']++;
+        if (isalpha(str2[i])) count[tolower(str2[i]) - 'a']--;
+    }
+
+    for (i = 0; i < 26; i++) {
+        if (count[i] != 0) return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    const char *word1 = "listen";
+    const char *word2 = "silent";
+
+    if (isAnagram(word1, word2)) {
+        printf("\"%s\" and \"%s\" are anagrams!\n", word1, word2);
+    } else {
+        printf("\"%s\" and \"%s\" are NOT anagrams.\n", word1, word2);
+    }
+
+    return 0;
+}
+*/
+
+//7
+/**/
